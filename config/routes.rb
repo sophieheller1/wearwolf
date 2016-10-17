06b72a7822f8b11d2b0615dc19e71cda66815960
Sophie Heller <sophieheller1@gmail.com>
2016-10-17 16:37:26 -0400

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root"locations#index"
   resources :locations
+
+
+  
   #
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 end
