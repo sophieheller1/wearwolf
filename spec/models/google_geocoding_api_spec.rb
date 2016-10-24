ea96da07f31ac47fcf15f0145f9347d4fe69075b
Sophie Heller <sophieheller1@gmail.com>
2016-10-24 14:26:20 -0400

    Configured VCR

--- spec/models/google_geocoding_api_spec.rb
+++ spec/models/google_geocoding_api_spec.rb
@@ -1,1 +1,13 @@
-require 'rails-helper'
+require 'rails_helper'
+require 'net/http'
+
+describe GoogleGeocoding do
+  it "returns the latitude and longitude of an entered zip" do
+    VCR.use_cassette("google_geocoding") do
+      uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=02143&key=#{ENV['GOOGLE_MAPS_GEOCODING_API_KEY']}")
+      response = Net::HTTP.get_response(uri)
+      expect(response.data['results']['address_components']['geometry']['location']['lat']).to eq 42.3808757
+      expect(response.data['results']['address_components']['geometry']['location']['lng']).to eq -71.1002881
+    end
+  end
+end
