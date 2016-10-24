6ab970a8766ffb7ae01e5514a4439893c18ce400
Sophie Heller <sophieheller1@gmail.com>
2016-10-24 14:45:25 -0400

    Location unit tests passing

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe Location, type: :model do
+  it { should have_valid(:city).when('Somerville', 'Rochester') }
+  it { should_not have_valid(:city).when('', nil) }
+  it { should have_valid(:state).when('Massachussetts', 'MA') }
+  it { should_not have_valid(:state).when('', nil) }
+  it { should have_valid(:zip).when('02143', '07626') }
+  it { should_not have_valid(:zip).when('', nil) }
+  it { should have_valid(:country).when('United States', 'USA') }
+  it { should_not have_valid(:country).when('', nil) }
+
+  describe '.city' do
+    it 'returns the city of the location' do
+      location = FactoryGirl.create(:location, city: "Cresskill")
+      city = location.city
+
+      expect(city).to eq("Cresskill")
+    end
+  end
+
+  describe '.state' do
+    it 'returns the state of the location' do
+      location = FactoryGirl.create(:location, state: "Mordor")
+      state = location.state
+
+      expect(state).to eq("Mordor")
+    end
+  end
+
+  describe '.zip' do
+    it 'returns the zip of the location' do
+      location = FactoryGirl.create(:location, zip: "01234")
+      zip = location.zip
+
+      expect(zip).to eq("01234")
+    end
+  end
+
+  describe '.country' do
+    it 'returns the country of the location' do
+      location = FactoryGirl.create(:location, country: "Valor")
+      country = location.country
+
+      expect(country).to eq("Valor")
+    end
+  end
+end
