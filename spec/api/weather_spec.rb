71a5826437315cf29256dc25ef0cf0ff36aad7e3
Sophie Heller <sophieheller1@gmail.com>
2016-10-25 11:49:32 -0400

    Resolve Merge Conflict

--- spec/api/weather_spec.rb
+++ spec/api/weather_spec.rb
@@ -4,6 +4,7 @@
 # feature 'google results', vcr: true do
 #   let!(:user) { FactoryGirl.create(:user) }
 #   let!(:location) { FactoryGirl.create(:location, user: user, latitude: 42.3808757, longitude: -71.1002881) }
+
 #
 #   scenario 'google api makes call to find weather' do
 #     visit "/locations/#{location.id}"
