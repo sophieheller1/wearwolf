a0dffed5a1a88e80cdf621f44e36e93c07add6bc
lraccomando <lraccomando@gmail.com>
2016-10-27 21:41:30 -0400

    Updates to README

--- spec/features/03_auth_user_can_update_profile_spec.rb
+++ spec/features/03_auth_user_can_update_profile_spec.rb
@@ -17,20 +17,20 @@ describe 'Auth user can update profile' do
       expect(page).to have_button 'Your profile'
     end
 
-    scenario 'User changes favorite location' do
+    scenario 'User changes favorite vacation spot' do
       visit '/'
       fill_in 'Email', with: user_1.email
       fill_in 'Password', with: user_1.password
       click_button 'Log in'
       click_button 'Your profile'
-      fill_in 'Favorite location', with: 'Boston'
+      fill_in 'Favorite Vacation Spot', with: 'Boston'
       choose 'cold'
       click_button 'Update your profile'
       expect(page).to have_content 'Profile updated successfully.'
 
       click_button 'Your profile'
 
-      find_field('Favorite location').value.should eq 'Boston'
+      find_field('Favorite Vacation Spot').value.should eq 'Boston'
       expect(page).to have_content 'cold'
 
     end
--- spec/features/06_auth_user_sees_location_weather_spec.rb
+++ spec/features/06_auth_user_sees_location_weather_spec.rb
@@ -2,9 +2,48 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'User can' do
-  feature 'visit a location and see the weather' do
-    scenario 'user visits location' do
+  let!(:user_1) { FactoryGirl.create(:user) }
+  let!(:location_1) { FactoryGirl.create(:location, user_id: user_1.id) }
 
+  feature 'visit a location and see the clothing suggestions' do
+    scenario 'user visits location' do
+      visit '/'
+      fill_in 'Email', with: user_1.email
+      fill_in 'Password', with: user_1.password
+      click_button 'Log in'
+
+      click_link location_1.city
+
+      expect(page).to have_content location_1.city
+      expect(page).to have_content 'Click for wolf advice'
     end
+
+   scenario 'user clicks to see weather' do
+   	  visit '/'
+      fill_in 'Email', with: user_1.email
+      fill_in 'Password', with: user_1.password
+      click_button 'Log in'
+
+      click_link location_1.city
+      click_button 'Wolf Advice'
+      wait_for_ajax
+
+      expect(page).to have_content('High')
+      expect(page).to have_content('Humidity')
+      expect(page).to not_have selector('wolf-image')
+   end
+
+   scenario 'user sees wolf advice' do
+	  visit '/'
+      fill_in 'Email', with: user_1.email
+      fill_in 'Password', with: user_1.password
+      click_button 'Log in'
+
+      click_link location_1.city
+      click_button 'Wolf Advice'
+      wait_for_ajax
+
+      expect(page).to have_selector('wolf-advice')
+   end
   end
 end
